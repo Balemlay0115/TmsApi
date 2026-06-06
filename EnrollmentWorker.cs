2698f1ae5733dcaea5bf9ddfb2eafde24b38cf07
Ba15 <Balemlay15@gmail.com>
2026-06-06 02:26:30 -0700

    this is a new change

--- EnrollmentWorker.cs
+++ EnrollmentWorker.cs
@@ -1,20 +1,2 @@
 
-builder.Services.AddSingleton<EnrollmentWorker>();
 
-builder.Services.AddScoped<IEnrollmentService,EnrollmentService>();
-
-builder.Host.UseDefaultServiceProvider(options=>
-{
-    options.ValidateScopes=true;
-    options.ValidateOnBuild=true;
-});
-app.MapGet("/api/enrollments/worker-smoke",(EnrollmentWorker worker)=>{
-    worker.ProcessBatch();
-    return Result.Ok("processed");
-
-});
-$base="http://localhost:5086"
-1..15|ForEach-object - parallel{
-    Invoke-WebRequest -Uri"$using:base/api/enrollments/worker-smoke"
--UseBasicParsing | Out-Null}
--ThrottleLimit 15
--- Program.cs
+++ Program.cs
@@ -1,4 +1,13 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddSingleton<EnrollmentWorker>();
+
+builder.Services.AddScoped<IEnrollmentService,EnrollmentService>();
+
+builder.Host.UseDefaultServiceProvider(options=>
+{
+    options.ValidateScopes=true;
+    options.ValidateOnBuild=true;
+});
 
 // Add services to the container.
 
@@ -21,7 +30,11 @@ app.MapGet("/api/assessments/results",()=>Results.Ok(new
     studentId="S-001",
     letterGrade="A"
 }));
+app.MapGet("/api/enrollments/worker-smoke",(EnrollmentWorker worker)=>{
+    worker.ProcessBatch();
+    return Result.Ok("processed");
 
+});
 // app.MapControllers();
 
 app.Run();
