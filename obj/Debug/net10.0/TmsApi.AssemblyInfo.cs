75a6b84d6ff02b05eb69a3ed57cbf71463a582af
Balemlay0115 <https://github.com/Balemlay0115/TmsApi.git>
2026-06-23 11:31:01 +0300

    added new thing

--- obj/Debug/net10.0/TmsApi.AssemblyInfo.cs
+++ obj/Debug/net10.0/TmsApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TmsApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b88c362fb3ae62b8c593ff6351c6d4a65a44ae2a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16623963a2f6bb78877645b1b5d2eaf3c7fbc1a0")]
 [assembly: System.Reflection.AssemblyProductAttribute("TmsApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TmsApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
