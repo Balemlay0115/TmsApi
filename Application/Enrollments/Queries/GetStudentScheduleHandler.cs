97fee959fc44b0bd895b64bd614449f5d5a4c996
Balemlay0115 <Balemlay0115@gmail.com>
2026-07-16 11:10:38 +0300

    extra touch

---
+++ Application/Enrollments/Queries/GetStudentScheduleHandler.cs
@@ -0,0 +1,23 @@
+using MediatR;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using TmsApi.Services;
+
+namespace TmsApi.Application.Enrollments.Queries;
+
+public class GetStudentScheduleHandler(IEnrollmentService repo)
+    : IRequestHandler<GetStudentScheduleQuery, ScheduleDto>
+{
+    public async Task<ScheduleDto> Handle(
+        GetStudentScheduleQuery query, CancellationToken ct)
+    {
+        var enrollments = await repo.GetByStudentIdAsync(query.StudentId, ct);
+        var items = enrollments.Select(e => new ScheduleItemDto(
+            e.Course.Code,
+            e.Course.Title,
+            "TBD")).ToList();
+
+        return new ScheduleDto(query.StudentId, items);
+    }
+}
